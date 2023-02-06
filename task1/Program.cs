6c7cf3dd76eafda78a51fd7eab192066220e64fa
«Natalia <kolodina_melbyk@mail.ru>
2023-02-06 12:18:57 +0400

    add task1 types of method

---
+++ task1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Виды методов:
+
+// Вид 1
+// - не принимает
+// - не возвращает
+void Method1()
+{
+    Console.WriteLine("Автор: ....");
+}
+
+// Вызвать метод:
+Method1();
+
+// Вид2
+// + принимает
+// - не возвращает
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Вызвать метод:
+Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+// // Вызвать метод:
+Method21("Текст", 4);
+
+// // или
+// Method21(msg: "Текст", count: 4);
+// //или
+// Method(count: 4, msg: "Текст");
+
+// Вид 3
+// - не принимает
+// + возвращает
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+// Вызвать метод:
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4
+// + принимает
+// + возвращает
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+//Вызвать метод:
+string res = Method4(10, "b");
+Console.WriteLine(res);
